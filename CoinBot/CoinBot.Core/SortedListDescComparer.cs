714fe3f49a99e8ad178c1bf43c5c48586135a90c
Matt Scheetz <mfscheetz@gmail.com>
2018-05-12 22:15:26 -0400

    generic sorted list sorter

---
+++ CoinBot/CoinBot.Core/SortedListDescComparer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoinBot.Core
+{
+    /// <summary>
+    /// Sort SortedList descending
+    /// </summary>
+    /// <typeparam name="T">Generic Type</typeparam>
+    public class SortedListDescComparer<T> : IComparer<T>
+    {
+        public int Compare(T x, T y)
+        {
+            return Comparer<T>.Default.Compare(y, x);
+        }
+    }
+}
