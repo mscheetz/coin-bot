51e38726b175e03996dd2c0a4636501508bf6450
Matt Scheetz <mfscheetz@gmail.com>
2018-05-15 22:17:20 -0400

    File Repository

---
+++ CoinBot/CoinBot.Data.Interface/IFileRepository.cs
@@ -0,0 +1,30 @@
+﻿using CoinBot.Business.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoinBot.Data.Interface
+{
+    public interface IFileRepository
+    {
+        /// <summary>
+        /// Get BotSettings
+        /// </summary>
+        /// <returns>BotSettings object</returns>
+        BotSettings GetConfig();
+
+        /// <summary>
+        /// Update BotSettings file
+        /// </summary>
+        /// <param name="botSettings">Updated BotSettings</param>
+        /// <returns>Boolean when complete</returns>
+        bool UpdateBotSettings(BotSettings botSettings);
+
+        /// <summary>
+        /// Write transaction to log
+        /// </summary>
+        /// <param name="tradeInformation">TradeInformation to write</param>
+        /// <returns>Boolean when complete</returns>
+        bool LogTransaction(TradeInformation tradeInformation);
+    }
+}
---
+++ CoinBot/CoinBot.Data/FileRepository.cs
@@ -0,0 +1,70 @@
+﻿using CoinBot.Business.Entities;
+using CoinBot.Data.Interface;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CoinBot.Data
+{
+    public class FileRepository : IFileRepository
+    {
+        private string configPath = "config.json";
+        private string transactionPath = "transaction.log";
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public FileRepository()
+        {
+        }
+
+        /// <summary>
+        /// Get BotSettings
+        /// </summary>
+        /// <returns>BotSettings object</returns>
+        public BotSettings GetConfig()
+        {
+            using (StreamReader r = new StreamReader(configPath))
+            {
+                string json = r.ReadToEnd();
+
+                BotSettings settings = JsonConvert.DeserializeObject<BotSettings>(json);
+
+                return settings;
+            }
+        }
+
+        /// <summary>
+        /// Update BotSettings file
+        /// </summary>
+        /// <param name="botSettings">Updated BotSettings</param>
+        /// <returns>Boolean when complete</returns>
+        public bool UpdateBotSettings(BotSettings botSettings)
+        {
+            var json = JsonConvert.SerializeObject(botSettings);
+
+            File.WriteAllText(configPath, json);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Write transaction to log
+        /// </summary>
+        /// <param name="tradeInformation">TradeInformation to write</param>
+        /// <returns>Boolean when complete</returns>
+        public bool LogTransaction(TradeInformation tradeInformation)
+        {
+            var json = JsonConvert.SerializeObject(tradeInformation);
+
+            using (StreamWriter s = File.AppendText(transactionPath))
+            {
+                s.WriteLine(json);
+
+                return true;
+            }
+        }
+    }
+}
