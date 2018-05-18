b9ad6f7bda09a57bfb6144af63a37259b0d74540
Matt Scheetz <mfscheetz@gmail.com>
2018-05-17 21:57:25 -0400

    DI and controller updates; swagger

--- CoinBot/CoinBot.Api/Controllers/CoinBotController.cs
+++ CoinBot/CoinBot.Api/Controllers/CoinBotController.cs
@@ -1,15 +1,96 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
+using CoinBot.Business.Entities;
+using CoinBot.Service;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoinBot.Api.Controllers
 {
     [Produces("application/json")]
-    [Route("api/CoinBot")]
+    [Route("api/coinbot")]
     public class CoinBotController : Controller
     {
+        private ICoinBotService _service;
+
+        public CoinBotController(ICoinBotService service)
+        {
+            _service = service;
+        }
+
+        /// <summary>
+        /// Update bot settings
+        /// </summary>
+        /// <param name="botSettings">New bot settings</param>
+        /// <returns>Boolean value when complete</returns>
+        /// POST: api/coinbot/settings
+        [HttpPost("settings")]
+        public bool UpdateBotSettings(BotSettings botSettings)
+        {
+            return _service.UpdateBotSettings(botSettings);
+        }
+
+        /// <summary>
+        /// Start trading with 1 Minute interval on candlesticks
+        /// </summary>
+        /// <param name="interval">Candlestick interval</param>
+        /// GET: api/coinbot/start
+        [HttpGet("start")]
+        public bool StartBot()
+        {
+            return _service.StartBot(Interval.FiveM);
+        }
+
+        /// <summary>
+        /// Start trading
+        /// </summary>
+        /// <param name="interval">Candlestick interval</param>
+        /// GET: api/coinbot/start/{interval}
+        [HttpGet("start/{interval}")]
+        public bool StartBot(Interval interval)
+        {
+            return _service.StartBot(interval);
+        }
+
+        /// <summary>
+        /// Stop Trading
+        /// </summary>
+        /// GET: api/coinbot/stop
+        [HttpGet("stop")]
+        public bool StopBot()
+        {
+            return _service.StopBot();
+        }
+
+        /// <summary>
+        /// Get all transactions
+        /// </summary>
+        /// <returns>Collection of TradeInformation</returns>
+        /// GET: api/coinbot/transactions
+        [HttpGet("transactions")]
+        public IEnumerable<TradeInformation> GetTransactionHistory()
+        {
+            return _service.GetTransactionHistory();
+        }
+
+        /// <summary>
+        /// Get current balance
+        /// </summary>
+        /// <returns>BotBalance object</returns>
+        /// GET: api/coinbot/balance
+        [HttpGet("balance")]
+        public BotBalance GetBalance()
+        {
+            return _service.GetBalance();
+        }
+
+        /// <summary>
+        /// Get Balance history
+        /// </summary>
+        /// <returns>Collection of BotBalance objects</returns>
+        /// GET: api/coinbot/balance/history
+        [HttpGet("balance/history")]
+        public IEnumerable<BotBalance> GetBalanceHistory()
+        {
+            return _service.GetBalanceHistory();
+        }
     }
 }
--- CoinBot/CoinBot.Api/Startup.cs
+++ CoinBot/CoinBot.Api/Startup.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoinBot.Business.Builders;
+using CoinBot.Business.Builders.Interface;
+using CoinBot.Data;
+using CoinBot.Data.Interface;
+using CoinBot.Manager;
+using CoinBot.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace CoinBot.Api
 {
@@ -24,6 +31,23 @@ namespace CoinBot.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddScoped<IBinanceRepository, BinanceRepository>();
+            services.AddScoped<IFileRepository, FileRepository>();
+            services.AddTransient<ITradingBuilder, TradingBuilder>();
+            services.AddTransient<ICoinBotService, CoinBotManager>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "CoinBot API",
+                    Description = "RESTful API endpoints for CoinBot",
+                    Contact = new Contact { Name = "CryptoBitfolio", Email = "CryptoBitfolio@gmail.com", Url = "https://twitter.com/CryptoBitfolio" },
+                    Version = "1.0"
+
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +57,13 @@ namespace CoinBot.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CoinBot API V1");
+            });
 
             app.UseMvc();
         }
