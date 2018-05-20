ab96334e03c7e4ea584877a5620e49f45f0a7e7a
Matt Scheetz <mfscheetz@gmail.com>
2018-05-19 20:34:38 -0400

    Updated helper elements

--- CoinBot/CoinBot.Core/Helper.cs
+++ CoinBot/CoinBot.Core/Helper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CoinBot.Core
@@ -17,6 +18,58 @@ namespace CoinBot.Core
             var power = Convert.ToDecimal(Math.Pow(10, decimalPlaces));
 
             return Math.Floor(number * power) / power;
+        }
+
+        /// <summary>
+        /// Calculate buy percent difference
+        /// </summary>
+        /// <param name="currentPrice">Current price</param>
+        /// <param name="lastSell">Last sell price</param>
+        /// <returns>Double of percent difference</returns>
+        public double GetBuyPercent(decimal currentPrice, decimal lastSell)
+        {
+            return GetPercent(currentPrice, lastSell);
+        }
+
+        /// <summary>
+        /// Calculate sell percent difference
+        /// </summary>
+        /// <param name="currentPrice">Current price</param>
+        /// <param name="lastBuy">Last buy price</param>
+        /// <returns>Double of percent difference</returns>
+        public double GetSellPercent(decimal currentPrice, decimal lastBuy)
+        {
+            return GetPercent(lastBuy, currentPrice);
+        }
+
+        /// <summary>
+        /// Calculate percent difference from current price and last buy
+        /// </summary>
+        /// <param name="priceA">Price A</param>
+        /// <param name="priceB">Price B</param>
+        /// <returns>double of percent difference</returns>
+        public double GetPercent(decimal priceA, decimal priceB)
+        {
+            return priceB == 0 ? 0 : (double)(priceA / priceB) - 1;
+        }
+
+        /// <summary>
+        /// Get zero'd satoshi count
+        /// </summary>
+        /// <returns>decimal of zero sats</returns>
+        public decimal ZeroSats()
+        {
+            return 0.00000000M;
+        }
+
+        /// <summary>
+        /// Get a collection from an enum
+        /// </summary>
+        /// <typeparam name="T">Type of enum</typeparam>
+        /// <returns>Collection of enum values</returns>
+        public IEnumerable<T> GetEnumValues<T>()
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
         }
     }
 }
