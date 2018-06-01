b78efb764438e62ff4d56d28d36f896215ac7711
Matt Scheetz <mfscheetz@gmail.com>
2018-05-31 20:59:24 -0400

    GdaxTrade entity

---
+++ CoinBot/CoinBot.Business/GDAX/GdaxTrade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoinBot.Business.Entities
+{
+    public class GdaxTrade
+    {
+        public DateTime Time { get; set; }
+        public int TradeId { get; set; }
+        public decimal Price { get; set; }
+        public decimal Size { get; set; }
+        public string Side { get; set; }
+    }
+}
