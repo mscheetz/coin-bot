b8bef0dbcc4e76e2468e3e952dc5513f9d3412f5
Matt Scheetz <mfscheetz@gmail.com>
2018-06-02 00:19:11 -0400

    stop loss fix

--- CoinBot/CoinBot.Business.Builders/TradeBuilder.cs
+++ CoinBot/CoinBot.Business.Builders/TradeBuilder.cs
@@ -785,13 +785,13 @@ namespace CoinBot.Business.Builders
 
             var result = CancelTrade(tradeParams);
 
-            bool stopLossCanceled = true;
             var trade = new TradeResponse
             {
                 orderId = _openStopLossList[0].orderId,
                 clientOrderId = _openStopLossList[0].clientOrderId
             };
-            while (stopLossCanceled)
+            bool stopLossCanceled = false;
+            while (!stopLossCanceled)
             {
                 stopLossCanceled = CheckTradeStatus(trade);
             }
