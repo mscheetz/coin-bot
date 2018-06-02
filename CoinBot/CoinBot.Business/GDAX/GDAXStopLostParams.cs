6a8721ab32b5fe786b547a86018bb06f2707c8a8
Matt Scheetz <mfscheetz@gmail.com>
2018-06-02 00:54:05 -0400

    entities

---
+++ CoinBot/CoinBot.Business/GDAX/GDAXStopLostParams.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoinBot.Business.Entities
+{
+    public class GDAXStopLostParams : GDAXTradeParams
+    {
+        public string stop { get; set; }
+        public decimal stop_price { get; set; }
+    }
+}
---
+++ CoinBot/CoinBot.Business/GDAX/GDAXTradeParams.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoinBot.Business.Entities
+{
+    public class GDAXTradeParams
+    {
+        public string type { get; set; }
+        public decimal size { get; set; }
+        public decimal price { get; set; }
+        public string side { get; set; }
+        public string product_id { get; set; }
+    }
+}
