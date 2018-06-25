73a16b8d6641753be8cc6c723139e9b88916f3d4
Matt Scheetz <mfscheetz@gmail.com>
2018-06-24 22:29:46 -0400

    OrderBook detail object

---
+++ CoinBot/CoinBot.Business/BotEntities/OrderBookDetail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoinBot.Business.Entities
+{
+    public class OrderBookDetail
+    {
+        public decimal price { get; set; }
+        public int precision { get; set; }
+        public int position { get; set; }
+    }
+}
